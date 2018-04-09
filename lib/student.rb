1cc8df9948560cc8b045b550d2e06f34f281d815
Shun Yao <ysmiracle@hotmail.com>
2018-04-09 03:27:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(study)
+    @knowledge << study
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
